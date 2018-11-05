36fdf3ee9676be36322ec942ad426b7d881fda81
Tehreem Javid <tehreemjavid@Tehreems-MacBook-Pro-2.local>
2018-11-05 15:14:16 -0800

    added messages and toolbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Toolbar from './Components/Toolbar'
+import MessageList from './Components/MessageList'
+
+
 class App extends Component {
   render() {
     return (
     <div className="App-link" >
+      <Toolbar />
+      <MessageList />
+
 
       </div>
     );
---
+++ src/Components/Message.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+const Message = (props) => {
+    return (
+        
+        <div>
+          <div className="row message read">
+    <div className="col-xs-1">
+      <div className="row">
+        <div className="col-xs-2">
+          <input type="checkbox" />
+        </div>
+        <div className="col-xs-2">
+          <i className="star fa fa-star"></i>
+        </div>
+      </div>
+    </div>
+    <div className="col-xs-11">
+      <span className="label label-warning">dev</span>
+      <span className="label label-warning">gschool</span>
+      <a href="#">
+        Here is some message text that has a bunch of stuff
+      </a>
+    </div>
+  </div>
+        </div>
+      )
+}
+    
+  
+
+export default Message 
