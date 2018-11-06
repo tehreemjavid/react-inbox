01f29636d92b5587b89c79271360498fd33f9fd3
Tehreem Javid <tehreemjavid@Tehreems-MacBook-Pro-2.local>
2018-11-06 11:02:04 -0800

    added all the stars and made them either shadded or not

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -1,17 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-const Message = (props) => {
+const Message = ({message}) => {
     return (
         
         <div>
-          <div className={`row message ${props.message.read ? 'read' : 'unread'} `}>
+          <div className={`row message ${ message.read ? 'read' : 'unread' } `}>
     <div className="col-xs-1">
       <div className="row">
         <div className="col-xs-2">
-          <input type="checkbox" />
+          <input
+           type="checkbox" 
+          checked={ message.selected }
+           />
         </div>
         <div className="col-xs-2">
-          <i className="star fa fa-star"></i>
+          <i className={`star fa fa-star${ message.starred ? '' : '-o' }`}></i>
         </div>
       </div>
     </div>
